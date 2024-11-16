74c227e42db33bfab5637688dfdf711edec41ff9
sami <sami.anki1316@gmail.com>
2024-11-16 22:29:23 +0100

    feat: start sign-in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInCard from "@/features/auth/components/sign-in-card";
+
+export const SignInPage = () => {
+  return (
+    <div>
+      <SignInCard />
+    </div>
+  );
+};
+
+export default SignInPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
       <h1 className="font-semibold">Home Page</h1>
       <Button>Click me</Button>
       <Button variant="destructive">Click me</Button>
-
-      <p></p>
     </div>
   );
 }
