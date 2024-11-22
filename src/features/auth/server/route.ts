2ead464562fb8c55f2f3020ec3a98d2dc874e91f
sami <sami.anki1316@gmail.com>
2024-11-22 03:34:12 +0100

    feat: add sign up request with cookie

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,9 +1,13 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { setCookie } from "hono/cookie";
 import {
   loginSchema,
   registerSchema,
 } from "@/features/auth/components/schemas";
+import { createAdminClient } from "@/lib/appwriter";
+import { ID } from "node-appwrite";
+import { AUTH_COOKIE } from "../constantes";
 
 const authRoute = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
@@ -13,8 +17,19 @@ const authRoute = new Hono()
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
-    console.log("register", { email, password });
-    return c.json({ name, email, password });
+    const { account } = await createAdminClient();
+    const user = await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true, user });
   });
 
 export default authRoute;
